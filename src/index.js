bd441379c3838afc0d67eca6d12dad8f39c48cc7
Harshit Shukla <mrharshit2409@gmail.com>
2023-12-29 12:45:03 +0530

    Added roles and user_roles through tables

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const bodyParser = require('body-parser');
 const app = express();
+const db = require('./models/index');
 
 const prepareAndStartServer = () =>{
     
@@ -11,8 +12,11 @@ const prepareAndStartServer = () =>{
     
     app.use('/api', apiRoutes);    
     
-    app.listen(PORT , () => {
+    app.listen(PORT , async() => {
         console.log(`Server Started on PORT: ${PORT}`)
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true})
+        }
     })
 }
 prepareAndStartServer();
